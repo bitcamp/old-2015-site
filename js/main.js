e1203ec7b0582e6e8c46bb786fbeb1d108baaf9a
JVLareme <Noreplysplease@gmail.com>
2014-11-25 10:43:14 -0500

    robots.txt
    also limit 12 pics for instagram

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@
         clientId: 'f6888bcf96454d39ae8cd771e340e004',
         accessToken: '1240423695.467ede5.ee5c520eb9064b608ab179c869d5e17c',
         resolution: "standard_resolution",
-        template: '<li><a href="{{link}}"><img src="{{image}}" /></a></li>'
+        template: '<li><a href="{{link}}"><img src="{{image}}" /></a></li>',
+        limit:12
     });
     feed.run();
 })();
