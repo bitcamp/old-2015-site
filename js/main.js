c4a2b4a62163169d9b6eb4d0bf0f7dc8774fb5c5
JVLareme <Noreplysplease@gmail.com>
2014-11-25 10:02:05 -0500

    instagrammin'
    :rainbow:

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function() {
+    var feed = new Instafeed({
+        get: 'user',
+        userId: 1240423695,
+        clientId: 'f6888bcf96454d39ae8cd771e340e004',
+        accessToken: '1240423695.467ede5.ee5c520eb9064b608ab179c869d5e17c',
+        resolution: "standard_resolution",
+        template: '<li><a href="{{link}}"><img src="{{image}}" /></a></li>'
+    });
+    feed.run();
+})();
